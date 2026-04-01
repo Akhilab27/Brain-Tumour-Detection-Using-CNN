d950ed70ae862792b67177631f036cff4b6a9917
Akhilab27 <akhilab270@gmail.com>
2026-04-02 02:22:44 +0530

    Added model.weights

--- src/app.py
+++ src/app.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 import cv2
 import os
 from PIL import Image
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
